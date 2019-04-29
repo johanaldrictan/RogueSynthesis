97af95bc3aed9dee9179fbbc1aafb887e80a37c5
ArtlessAvian <ArtlessAvian@users.noreply.github.com>
2019-04-28 20:00:24 -0700

    WeightedTiles, Pathing with Weights

---
+++ Assets/Map/WeightedTile.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class WeightedTile : Tile
+{
+    [SerializeField]
+    public TileWeight weight;
+    // WeightedTile[] neighbors = new WeightedTile[4];
+
+    // private void doNeighborLink(WeightedTile other)
+    // {
+    //     this.neighbors
+    // }
+
+    // public override void RefreshTile(Vector3Int location, ITilemap tilemap)
+    // {
+    //     TileBase north = tilemap.GetTile(location + new Vector3Int(1, 0, 0));
+    //     if (north is WeightedTile) { doNeighborLink(north as WeightedTile); }
+    // }
+
+    #if UNITY_EDITOR
+    [MenuItem("Assets/Create/WeightedTile")]
+    public static void CreateRoadTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Weighted Tile", "New Weighted Tile", "Asset", "Save Weighted Tile", "Assets");
+        if (path == "")
+            return;
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<WeightedTile>(), path);
+    }
+    #endif
+}
+
+public enum TileWeight
+{
+    //grass
+    UNOBSTRUCTED = 1,
+    LIGHT_OBS = 2,
+    MEDIUM_OBS = 3,
+    HEAVY_OBS = 4,
+    //mountains, other units
+    OBSTRUCTED = 99
+}
