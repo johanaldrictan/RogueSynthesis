01b371b345699ccdc49201c3db132b3c27146577
Unknown <ajpark5@uci.edu>
2019-07-10 19:31:24 -0700

    More work on UI elements

---
+++ Assets/UI_Operator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Operator : MonoBehaviour
+{
+
+    public GameObject rightClickPanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        if (Input.GetMouseButtonDown(1))
+        {
+            rightClickPanel.SetActive(true);
+            //var mouseCoord = Vector3(Input.mousePosition);
+            Debug.Log(Input.mousePosition);
+        }
+    }
+}
