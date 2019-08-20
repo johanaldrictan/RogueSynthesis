e64509a4aec8735bfe451f8b29eb561bfede1b7c
Unknown <ajpark5@uci.edu>
2019-08-20 15:54:25 -0700

    Loading screen + build settings changes
    loading screen is now operational and also has hint text functionality

---
+++ Assets/_Game/UI/Hint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Hint : MonoBehaviour
+{
+    public TextMeshProUGUI hintText;
+
+    [TextArea(3, 10)]
+    public string[] sentences;
+    private int index;
+
+    private int randomizer;
+
+    public void Start()
+    {
+        randomizer = Random.Range(0, sentences.Length);
+        hintText.text = sentences[randomizer];
+    }
+
+
+}
