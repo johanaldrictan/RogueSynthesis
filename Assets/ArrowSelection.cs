620d668e4cab6896dd3787a70d496230d5868b0e
Unknown <ajpark5@uci.edu>
2019-12-03 15:40:23 -0800

    Arrow clicking functional

--- Assets/ArrowSelection.cs
+++ Assets/ArrowSelection.cs
@@ -22,6 +22,22 @@ public class ArrowSelection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                //hit.collider.attachedRigidbody.AddForce(Vector2.up);
+            }
+
+        }
+
+        
+        /*
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         //Debug.Log(ObjectHitByMouse.collider.gameObject);
@@ -39,5 +55,6 @@ public class ArrowSelection : MonoBehaviour
             }
 
         }
+        */
     }
 }
